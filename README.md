430a3bbb92e298179e066e997b2abe4f25006992
Adam Nagy <adam_e_nagy@hotmail.com>
2015-09-28 14:30:10 +0100

    Update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FusionUtils
 Some utils including a shell script to remove all Fusion folders
+
+- RemoveFusionFiles
+
+This can be used for doing a clean uninstall - i.e. once you uninstalled fusion remove all the additional folder and files from your Mac
+
+You probably need to run it with the sudo option in the Terminal so it has all necessary privileges:
+
+sudo sh RemoveFusionFiles.sh 
---
+++ RemoveFusionFiles/RemoveFusionFiles.sh
@@ -0,0 +1,22 @@
+rm -rf ~/'Library/Application Support/Autodesk/Autodesk Fusion 360'
+rm -rf ~/'Library/Application Support/Autodesk/MC3'
+rm -rf ~/'Library/Application Support/Autodesk/Synergy'
+rm -rf ~/'Library/Application Support/Autodesk/Neutron'
+rm -rf ~/'Library/Application Support/Autodesk/Neutron Platform'
+rm -rf ~/'Library/Application Support/Autodesk/NINVFUS'
+rm -rf ~/'Library/Application Support/Autodesk/production'
+rm -rf ~/'Library/Application Support/Autodesk/Web Services'
+rm -rf ~/'Library/Application Support/Autodesk/Webdeploy'
+
+rm -rf ~/'Library/Application Support/Autodesk/Common/Material Library'
+
+rm -rf ~/'Library/Caches/com.autodesk.senddump'
+rm -rf ~/'Library/Logs/autodesk.webdeploy.streamer.log'
+rm -rf ~/'Library/Preferences/com.autodesk.Autodesk Fusion 360.plist'
+rm -rf ~/'Library/Preferences/com.autodesk.fusion360.plist'
+rm -rf ~/'Library/Preferences/com.autodesk.MC3Framework.plist'
+rm -rf ~/'Library/CrashReporter/MaterialLibraryMigrator.plist'
+
+rm -rf $TMPDIR/Autodesk
+rm -rf $TMPDIR/Neutron
+rm -rf $TMPDIR/proteinrun
